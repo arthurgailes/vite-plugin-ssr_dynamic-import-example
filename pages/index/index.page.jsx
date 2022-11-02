21fb236dea80e07ae61ab85fe3bbaa115d90292e
Arthur Gailes <agailes1@gmail.com>
2022-11-02 08:26:06 -0400

    remove extraneous code

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-export { Page }
+import React from "react";
+export { Page };
 
-import './index.css'
-
-const Loading = () => <div>Loading</div>
+const Loading = () => <div>Loading</div>;
 function Page() {
   return (
     <>
@@ -12,20 +10,20 @@ function Page() {
       <ul>
         <li>Rendered to HTML and hydrated in the browser.</li>
         <li>
-          Interactive - only loaded on client: <ClientSideComponent />
+          Interactive - this component is loaded dynamically on client:{" "}
+          <ClientSideComponent />
         </li>
       </ul>
-      <p className="colored ssr">Blue text.</p>
     </>
-  )
+  );
 }
 
 function ClientSideComponent() {
   // We lazily load the client-side component
-  const Counter = React.lazy(() => import('./Counter'))
+  const Counter = React.lazy(() => import("./Counter"));
   return (
     <React.Suspense fallback={<Loading />}>
       <Counter />
     </React.Suspense>
-  )
+  );
 }
--- renderer/PageLayout.jsx
+++ renderer/PageLayout.jsx
@@ -1,7 +1,7 @@
-export { PageLayout }
+export { PageLayout };
 
-import './PageLayout.css'
-import React from 'react'
+import "./PageLayout.css";
+import React from "react";
 
 function PageLayout({ children }) {
   return (
@@ -10,52 +10,23 @@ function PageLayout({ children }) {
         <Content>{children}</Content>
       </Layout>
     </React.StrictMode>
-  )
+  );
 }
 
 function Layout({ children }) {
   return (
     <div
       style={{
-        display: 'flex',
+        display: "flex",
         maxWidth: 900,
-        margin: 'auto'
+        margin: "auto",
       }}
     >
       {children}
     </div>
-  )
-}
-
-function Sidebar({ children }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        paddingTop: 42,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        lineHeight: '1.8em'
-      }}
-    >
-      {children}
-    </div>
-  )
+  );
 }
 
 function Content({ children }) {
-  return (
-    <div
-      style={{
-        padding: 20,
-        paddingBottom: 50,
-        borderLeft: '2px solid #eee',
-        minHeight: '100vh'
-      }}
-    >
-      {children}
-    </div>
-  )
+  return <div>{children}</div>;
 }
