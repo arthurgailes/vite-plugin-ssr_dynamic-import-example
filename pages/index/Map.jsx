1d4a27af2c3427860a8db039bafb687a5d08a36a
Arthur Gailes <agailes1@gmail.com>
2022-12-19 15:47:35 -0500

    enable map fix

--- pages/index/Map.jsx
+++ pages/index/Map.jsx
@@ -1,10 +1,12 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 
-export default Map = () => (
-  <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-  </MapContainer>
+export default () => (
+  <div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  </div>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
 
 export default {
-  plugins: [react(), ssr({ includeAssetsImportedByServer: true })]
-}
+  plugins: [
+    react(),
+    ssr({ includeAssetsImportedByServer: true, prerender: true }),
+  ],
+};
